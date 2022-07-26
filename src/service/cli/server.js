5c0f699be4036611f7c555ad9c022feda1ac0c09
mikhailkaryamin <karyamin@uchi.ru>
2022-07-26 21:48:28 +0300

    add express and router to server

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,56 +1,26 @@
 'use strict';
 
-const http = require(`http`);
+const express = require(`express`);
+const {Router} = express;
 const fs = require(`fs`).promises;
-const chalk = require(`chalk`);
-const {DEFAULT_PORT, HttpCode, MOCKS_PATH} = require(`../../constants`);
+const {DEFAULT_PORT, MOCKS_PATH} = require(`../../constants`);
 
-const wrapTemplate = (markup) => {
-  const template = `
-  <!Doctype html>
-    <html lang="ru">
-    <head>
-      <title>Typoteka</title>
-    </head>
-    <body>
-      ${markup}
-    </body>
-  </html>`.trim();
+const app = express();
+const mainRoutes = new Router();
 
-  return template;
-};
-
-const writeBaseHeader = (res) => {
-  res.writeHead(HttpCode.SUCCESS, {
-    'Content-Type': `text/html; charset=UTF-8`
-  });
-};
-
-const onClientServer = async (req, res) => {
-  const TEXT_NOT_FOUND = `Not found`;
-
-  switch (req.url) {
-    case `/` :
-      writeBaseHeader(res);
-
-      try {
-        const mocksFile = await fs.readFile(MOCKS_PATH);
-        const mocks = JSON.parse(mocksFile);
-        const titles = mocks.map((article) => `<li>${article.title}</li>`).join(``);
-
-        res.end(wrapTemplate(`<ul>${titles}</ul>`));
-      } catch (error) {
-        console.log(chalk.red(error));
-        res.end(wrapTemplate(TEXT_NOT_FOUND));
-      }
-
-      break;
-    default:
-      writeBaseHeader(res);
-
-      res.end(wrapTemplate(TEXT_NOT_FOUND));
+const onResponsePosts = async (req, res) => {
+  try {
+    const mocksFile = await fs.readFile(MOCKS_PATH);
+    const mocksJson = JSON.parse(mocksFile);
+    res.json(mocksJson);
+  } catch (err) {
+    res.send([]);
   }
 };
+
+app.use(express.json());
+app.use(`/`, mainRoutes);
+mainRoutes.get(`/posts`, (req, res) => onResponsePosts(req, res));
 
 module.exports = {
   name: `--server`,
@@ -58,14 +28,6 @@ module.exports = {
     const [userPort] = args;
 
     const port = parseInt(userPort, 10) || DEFAULT_PORT;
-
-    http.createServer(onClientServer)
-      .listen(port)
-      .on(`listening`, () => {
-        console.log(chalk.green(`Ожидаю соединений на ${port}`));
-      })
-      .on(`error`, ({message}) => {
-        console.log(chalk.red(`Ошибка при создании сервера: ${message}`));
-      });
+    app.listen(port);
   }
 };
